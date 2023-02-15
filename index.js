dfeb9c51ee1cf9f3319e1de8a45af33e19eeaa5f
kalyan <kummarikd@gmail.com>
2023-02-15 09:41:45 -0800

    changed

---
+++ index.js
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		<h2>My First Web page</h2>
+	</title>
+</head>
+<body>
+	<h2>My new web page</h2>
+	<p id="demo"></p>
+	<center>
+			
+		<div>
+			<label> First name</label>
+		<input type="number" class="input" value="values" placeholder="Enter first name">
+		</div>
+		<br>
+		<div>
+			<label>Last name</label>
+			<input type="number" class="input" name="last name" placeholder="Enter last name">
+			</div>
+				<br>
+					
+				<div>
+
+					<input type="button" name="submit" onclick="input" value="submit">
+			
+				</div>
+
+	</center>
+
+
+</body>
+</HTML>
+
+/*function subtractOfCount(){
+    let firstnumber =10;
+    let secondnumber= 5;
+    let subtract=firstnumber-secondnumber;
+    console.log('sum of two numbers: ${subtract}');
+
+}
+    function DivisiontOfCount(){
+    let firstnumber =10;
+    let secondnumber= 5;
+    let division=firstnumber-secondnumber;
+    console.log('sum of two numbers: ${division}');
+}
+function multiplicationOfCount(){
+    let firstnumber =10;
+    let secondnumber= 5;
+    let multiplication=firstnumber-secondnumber;
+    console.log('sum of two numbers: ${multiplication}');
+}
+
+function kalyandas(){
+    let firstnumber = 20;
+    let secondnumber= 10;
+
+    sumofcount()
+    subtractOfCount()
+    DivisiontOfCount()
+    multiplicationOfCount();
+}
+kalyandas();
